725bbe93f3df76fbdfa8decb0281024ee67b89ea
as1134 <as1134@vcm-29221.vm.duke.edu>
2022-12-04 21:58:54 -0500

    add

---
+++ client/buffer.h
@@ -0,0 +1,15 @@
+#ifndef FINALPROJECT_BUFFER_H
+#define FINALPROJECT_BUFFER_H
+
+#include <string>
+#include <vector>
+#include <utility>
+using namespace std;
+class buffer {
+public:
+
+    static uint32_t getWholeBufferLength(vector<char> buffer);
+    static uint32_t getContentLength(vector<char> buffer);
+};
+
+#endif
